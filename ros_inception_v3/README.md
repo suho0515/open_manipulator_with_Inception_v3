0374b16494fc43125edb84ddf8d1ea6e88912774
suho0515 <suho0515_@naver.com>
2019-11-14 15:01:38 +0900

    Add files via upload

---
+++ ros_inception_v3/README.md
@@ -0,0 +1,90 @@
+# ros_inception_v3
+
+This repository contains the codes to run tensorflow image recognition with Inception V3 network in ROS. Also, enable users to train their own network model for image recognition.
+
+## reference
+
+retrain tensorflow official image recognition(inception v3) model: 
+https://github.com/akshaypai/tfClassifier
+
+runing tensorflow official image recognition(inception v3) in ros: 
+https://github.com/OTL/rostensorflow
+
+## Developer
+
+*PouChun, Kung (k3083518729@gmail.com)
+
+## Usage & Tutorial
+
+Medium Notes: https://medium.com/@k3083518729/tensorflow-image-recognition-58b0ac77c263
+
+- Install TensorFlow (see [tensor flow install guide](https://www.tensorflow.org/install/install_linux))
+- Install ROS (see http://wiki.ros.org)
+
+ROS camera driver & dependencies install
+-------------------------------------------
+```bash
+$ sudo apt-get install ros-kinetic-cv-bridge ros-kinetic-opencv3
+```
+
+Install camera driver (for example:cv_camera)
+
+```bash
+$ sudo apt-get install ros-kinetic-cv-camera
+```
+
+TensorFlow install
+-------------------------------------------
+Please read official guide.
+https://www.tensorflow.org/install/install_linux
+
+image_recognition.py
+--------------------------------
+
+* publish: /result (std_msgs/String)
+* subscribe: /image (sensor_msgs/Image)
+
+Usage
+
+```bash
+$ roscore
+$ rosrun cv_camera cv_camera_node
+$ python image_recognition.py image:=/cv_camera/image_raw
+$ rostopic echo /result
+```
+
+Retrain your own model
+--------------------------------
+Download retrain code
+
+```bash
+$ git clone https://github.com/akshaypai/tfClassifier
+$ cd tfClassifier/image_classification
+```
+Download dataset(for example:flower_photos dataset)
+
+```bash
+cd ~
+curl -O http://download.tensorflow.org/example_images/flower_photos.tgz
+tar xzf flower_photos.tgz
+```
+Retrain
+
+```bash
+python retrain.py --model_dir ./inception --image_dir ~/flower_photos ./output --how_many_training_steps 1000
+```
+
+After retrain process finish you can find ./output.pb and ./labels.txt in /tmp folder .
+
+Move or copy them to ros_inception_v3 folder.
+
+Replace original ./output.pb and ./labels.txt file with new file.
+
+Start test
+
+```bash
+$ roscore
+$ rosrun cv_camera cv_camera_node
+$ python image_recognition.py image:=/cv_camera/image_raw
+$ rostopic echo /result
+```
---
+++ ros_inception_v3/image_recognition.py
@@ -0,0 +1,92 @@
+import rospy
+from sensor_msgs.msg import Image
+from std_msgs.msg import String
+from cv_bridge import CvBridge
+import cv2
+import numpy as np
+import tensorflow as tf
+import argparse
+
+## get graph
+with tf.gfile.FastGFile("./output_graph.pb", 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+    _ = tf.import_graph_def(graph_def, name='')
+
+## get label
+label_lines = [line.rstrip() for line
+                   	in tf.gfile.GFile("./output_labels.txt")]
+
+def setup_args():
+  parser = argparse.ArgumentParser()
+  # classify_image_graph_def.pb:
+  #   Binary representation of the GraphDef protocol buffer.
+  # imagenet_synset_to_human_label_map.txt:
+  #   Map from synset ID to a human readable string.
+  # imagenet_2012_challenge_label_map_proto.pbtxt:
+  #   Text representation of a protocol buffer mapping a label to synset ID.
+  parser.add_argument(
+      '--model_dir',
+      type=str,
+      default='/tmp/imagenet',
+      help="""\
+      Path to classify_image_graph_def.pb,
+      imagenet_synset_to_human_label_map.txt, and
+      imagenet_2012_challenge_label_map_proto.pbtxt.\
+      """
+  )
+  parser.add_argument(
+      '--image_file',
+      type=str,
+      default='',
+      help='Absolute path to image file.'
+  )
+  parser.add_argument(
+      '--num_top_predictions',
+      type=int,
+      default=5,
+      help='Display this many predictions.'
+  )
+  global FLAGS
+  FLAGS, unparsed = parser.parse_known_args()
+  return unparsed
+
+class RosTensorFlow():
+    def __init__(self):
+        self._session = tf.Session()
+
+        self._cv_bridge = CvBridge()
+
+        self._sub = rospy.Subscriber('image', Image, self.callback, queue_size=1)
+        self._pub = rospy.Publisher('/open_manipulator/result', String, queue_size=1)
+        self.score_threshold = rospy.get_param('~score_threshold', 0.1)
+        self.use_top_k = rospy.get_param('~use_top_k', 5)
+
+    def callback(self, image_msg):
+        cv_image = self._cv_bridge.imgmsg_to_cv2(image_msg, "bgr8")
+        # copy from
+        # classify_image.py
+        image_data = cv2.imencode('.jpg', cv_image)[1].tostring()
+        # Creates graph from saved GraphDef.
+        softmax_tensor = self._session.graph.get_tensor_by_name('final_result:0')
+        predictions = self._session.run(
+            softmax_tensor, {'DecodeJpeg/contents:0': image_data})
+        predictions = np.squeeze(predictions)
+        # Creates node ID --> English string lookup.
+       
+        top_k = predictions.argsort()[-self.use_top_k:][::-1]
+        for node_id in top_k:
+            human_string = label_lines[node_id]
+            score = predictions[node_id]
+            if score > self.score_threshold:
+                rospy.loginfo('%s (score = %.5f)' % (human_string, score))
+                self._pub.publish(human_string)
+
+    def main(self):
+        rospy.spin()
+
+if __name__ == '__main__':
+    setup_args()
+    rospy.init_node('rostensorflow')
+    tensor = RosTensorFlow()
+    tensor.main()
